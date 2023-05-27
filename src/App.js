9e37e9aefb574a143715a0313d82bddfcde09664
ManausK <1905246@kiit.ac.in>
2023-05-27 17:00:29 +0530

    updating calendar js

--- src/App.js
+++ src/App.js
@@ -3,5 +3,32 @@ import { generateDate } from "./util/calendar";
 
 export default function App() {
   console.log(generateDate());
-  return <div>App</div>;
+
+  const days = ["S", "M", "T", "W", "T", "F", "S"];
+
+  return (
+    <div className="w-96 h-96">
+      <div className="w-full grid grid-cols-7">
+        {days.map((day, index) => {
+          return (
+            <h1 key={index} className="h-14 grid place-content-center text-sm">
+              {day}
+            </h1>
+          );
+        })}
+      </div>
+      <div className="w-full grid grid-cols-7">
+        {generateDate().map(({ date, currentMonth, today }, index) => {
+          return (
+            <div
+              key={index}
+              className="h-14 border-t grid place-content-center"
+            >
+              <h1>{date.date()}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/util/calendar.js
+++ src/util/calendar.js
@@ -9,9 +9,31 @@ export const generateDate = (
 
   const arrayOfDate = [];
 
-  for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
-    arrayOfDate.push;
+  //create prefix date
+  for (let i = 0; i < firstDateOfMonth.day(); i++) {
+    arrayOfDate.push({ currentMonth: false, date: firstDateOfMonth.day(i) });
   }
 
-  return [firstDateOfMonth, lastDateOfMonth];
+  //generate current date
+  for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+    arrayOfDate.push({
+      date: firstDateOfMonth.date(i),
+      currentMonth: true,
+      today:
+        firstDateOfMonth.date(i).toDate().toDateString() ===
+        dayjs().toDate().toDateString(),
+    });
+  }
+
+  const remaining = 42 - arrayOfDate.length;
+
+  for (
+    let i = lastDateOfMonth.date() + 1;
+    i <= lastDateOfMonth.date() + remaining;
+    i++
+  ) {
+    arrayOfDate.push({ date: lastDateOfMonth.date(i), currentMonth: false });
+  }
+
+  return arrayOfDate;
 };
